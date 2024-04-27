cd4468507b48987ee8abd13ced3b6c01bf132876
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-04-28 04:36:48 +0500

    refixed deps & linters

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  root: true,
-  rules: {
-    "react/display-name": 0,
-  },
-  // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
-};
