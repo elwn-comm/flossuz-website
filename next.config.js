07e909e887195a77b28e2550940f1c24e2e8210f
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-04-28 04:26:37 +0500

    first version of website

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    domains: ["lab.basement.studio"],
+  },
+  rewrites: async () => [
+    {
+      source: "/:path*",
+      destination: `/:path*`,
+    }
+  ],
 };
 
-module.exports = nextConfig;
+module.exports = (_phase, { defaultConfig: _ }) => {
+  const plugins = [
+    withBundleAnalyzer({ enabled: process.env.ANALYZE === "true" }),
+  ];
+  return plugins.reduce((acc, plugin) => plugin(acc), { ...nextConfig });
+};
---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const siteURL = new URL(
+  process.env.NEXT_PUBLIC_SITE_URL as string | URL
+);
+
+export const siteOrigin = siteURL.origin;
