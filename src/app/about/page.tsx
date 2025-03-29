08d288469ff1772f4d2d98636d5fba4d51ac0b9c
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-03-30 02:11:27 +0500

    chore: about page updated

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,133 +1,122 @@
-import { type Metadata } from 'next'
-import Image from 'next/image'
+import { type Metadata } from "next";
+import Image from "next/image";
 
-import { Border } from '@/components/Border'
-import { ContactSection } from '@/components/ContactSection'
-import { Container } from '@/components/Container'
-import { FadeIn, FadeInStagger } from '@/components/FadeIn'
-import { GridList, GridListItem } from '@/components/GridList'
-import { PageIntro } from '@/components/PageIntro'
-import { PageLinks } from '@/components/PageLinks'
-import { SectionIntro } from '@/components/SectionIntro'
-import { StatList, StatListItem } from '@/components/StatList'
-import imageAngelaFisher from '@/images/team/angela-fisher.jpg'
-import imageBenjaminRussel from '@/images/team/benjamin-russel.jpg'
-import imageBlakeReid from '@/images/team/blake-reid.jpg'
-import imageChelseaHagon from '@/images/team/chelsea-hagon.jpg'
-import imageDriesVincent from '@/images/team/dries-vincent.jpg'
-import imageEmmaDorsey from '@/images/team/emma-dorsey.jpg'
-import imageJeffreyWebb from '@/images/team/jeffrey-webb.jpg'
-import imageKathrynMurphy from '@/images/team/kathryn-murphy.jpg'
-import imageLeonardKrasner from '@/images/team/leonard-krasner.jpg'
-import imageLeslieAlexander from '@/images/team/leslie-alexander.jpg'
-import imageMichaelFoster from '@/images/team/michael-foster.jpg'
-import imageWhitneyFrancis from '@/images/team/whitney-francis.jpg'
-import { loadArticles } from '@/lib/mdx'
+import { Border } from "@/components/Border";
+import { ContactSection } from "@/components/ContactSection";
+import { Container } from "@/components/Container";
+import { FadeIn, FadeInStagger } from "@/components/FadeIn";
+import { GridList, GridListItem } from "@/components/GridList";
+import { PageIntro } from "@/components/PageIntro";
+import { PageLinks } from "@/components/PageLinks";
+import { SectionIntro } from "@/components/SectionIntro";
+import { StatList, StatListItem } from "@/components/StatList";
+import imageAngelaFisher from "@/images/team/angela-fisher.jpg";
+import imageBenjaminRussel from "@/images/team/benjamin-russel.jpg";
+import imageBlakeReid from "@/images/team/blake-reid.jpg";
+import imageChelseaHagon from "@/images/team/chelsea-hagon.jpg";
+import imageDriesVincent from "@/images/team/dries-vincent.jpg";
+import imageEmmaDorsey from "@/images/team/emma-dorsey.jpg";
+import imageJeffreyWebb from "@/images/team/jeffrey-webb.jpg";
+import imageKathrynMurphy from "@/images/team/kathryn-murphy.jpg";
+import imageLeonardKrasner from "@/images/team/leonard-krasner.jpg";
+import imageLeslieAlexander from "@/images/team/leslie-alexander.jpg";
+import imageMichaelFoster from "@/images/team/michael-foster.jpg";
+import imageWhitneyFrancis from "@/images/team/whitney-francis.jpg";
+import { loadArticles } from "@/lib/mdx";
 
 function Culture() {
   return (
     <div className="mt-24 rounded-4xl bg-neutral-950 py-24 sm:mt-32 lg:mt-40 lg:py-32">
       <SectionIntro
-        eyebrow="Our culture"
-        title="Balance your passion with your passion for life."
+        eyebrow="Madaniyatimiz"
+        title="Ochiq manbali innovatsiyalar orqali o'z ishtiyoqingni kuchaytir."
         invert
       >
         <p>
-          We are a group of like-minded people who share the same core values.
+          Biz ochiq manba tamoyillari va hamkorlik orqali rivojlanishga sodiq
+          bo'lgan birdam jamoamiz.
         </p>
       </SectionIntro>
       <Container className="mt-16">
         <GridList>
-          <GridListItem title="Loyalty" invert>
-            Our team has been with us since the beginning because none of them
-            are allowed to have LinkedIn profiles.
+          <GridListItem title="Hamkorlik" invert>
+            Biz birgalikda rivojlanishga ishonamiz. Har bir loyiha ochiq
+            hamkorlik orqali rivojlanadi.
           </GridListItem>
-          <GridListItem title="Trust" invert>
-            We don’t care when our team works just as long as they are working
-            every waking second.
+          <GridListItem title="Ishonch" invert>
+            Biz uchun ochiqlik va ishonch asosiy mezondir. Jamoamiz har doim o'z
+            bilim va tajribasini ochiq holda baham ko'radi.
           </GridListItem>
-          <GridListItem title="Compassion" invert>
-            You never know what someone is going through at home and we make
-            sure to never find out.
+          <GridListItem title="Taraqqiyot" invert>
+            Innovatsiyalar va yangi bilimlarni izlash orqali doimo oldinga
+            intilamiz va jamoamiz a'zolarini qo'llab-quvvatlaymiz.
           </GridListItem>
         </GridList>
       </Container>
     </div>
-  )
+  );
 }
 
 const team = [
   {
-    title: 'Leadership',
+    title: "Yetakchi Jamoa",
     people: [
       {
-        name: 'Leslie Alexander',
-        role: 'Co-Founder / CEO',
+        name: "Orzklv",
+        role: "Asoschi / Yetakchi",
+        link: "https://orzklv.uz",
         image: { src: imageLeslieAlexander },
       },
       {
-        name: 'Michael Foster',
-        role: 'Co-Founder / CTO',
-        image: { src: imageMichaelFoster },
+        name: "Otabek Ismoilov",
+        role: "Rais / Yetakchi",
+        link: "https://github.com/ismoilovdevml",
+        image: { src: imageDriesVincent },
       },
       {
-        name: 'Dries Vincent',
-        role: 'Partner & Business Relations',
+        name: "Kei Lissimus",
+        role: "Rais / Yetakchi",
+        link: "https://github.com/thelissimus",
         image: { src: imageDriesVincent },
       },
     ],
   },
   {
-    title: 'Team',
+    title: "Hamjamiyat Boshqarmalari",
     people: [
       {
-        name: 'Chelsea Hagon',
-        role: 'Senior Developer',
+        name: "Shahzod Qudratov",
+        role: "Hamjamiyatlar Boshqaruvi",
+        link: "https://shakhzod.me",
         image: { src: imageChelseaHagon },
       },
       {
-        name: 'Emma Dorsey',
-        role: 'Senior Designer',
+        name: "Bahrom Rahmatov",
+        role: "Mahalliylashtirish Raisi",
+        link: "https://bahrom04.github.io",
         image: { src: imageEmmaDorsey },
       },
       {
-        name: 'Leonard Krasner',
-        role: 'VP, User Experience',
-        image: { src: imageLeonardKrasner },
-      },
-      {
-        name: 'Blake Reid',
-        role: 'Junior Copywriter',
+        name: "Bobomurod Mo'minov",
+        role: "'Awesome' Boshqaruvi",
+        link: "https://bobomurod.uz",
         image: { src: imageBlakeReid },
       },
       {
-        name: 'Kathryn Murphy',
-        role: 'VP, Human Resources',
+        name: "Ahmad Qodirov",
+        role: "DevOps Journey",
+        link: "https://orzklv.uz",
+        image: { src: imageLeonardKrasner },
+      },
+      {
+        name: "Manuchehr Usmonov",
+        role: "VP, Human Resources",
+        link: "https://manu.uz",
         image: { src: imageKathrynMurphy },
-      },
-      {
-        name: 'Whitney Francis',
-        role: 'Content Specialist',
-        image: { src: imageWhitneyFrancis },
-      },
-      {
-        name: 'Jeffrey Webb',
-        role: 'Account Coordinator',
-        image: { src: imageJeffreyWebb },
-      },
-      {
-        name: 'Benjamin Russel',
-        role: 'Senior Developer',
-        image: { src: imageBenjaminRussel },
-      },
-      {
-        name: 'Angela Fisher',
-        role: 'Front-end Developer',
-        image: { src: imageAngelaFisher },
       },
     ],
   },
-]
+];
 
 function Team() {
   return (
@@ -149,23 +138,25 @@ function Team() {
                 >
                   {group.people.map((person) => (
                     <li key={person.name}>
-                      <FadeIn>
-                        <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
-                          <Image
-                            alt=""
-                            {...person.image}
-                            className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
-                          />
-                          <div className="absolute inset-0 flex flex-col justify-end bg-linear-to-t from-black to-black/0 to-40% p-6">
-                            <p className="font-display text-base/6 font-semibold tracking-wide text-white">
-                              {person.name}
-                            </p>
-                            <p className="mt-2 text-sm text-white">
-                              {person.role}
-                            </p>
+                      <a href={person.link}>
+                        <FadeIn>
+                          <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
+                            <Image
+                              alt=""
+                              {...person.image}
+                              className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
+                            />
+                            <div className="absolute inset-0 flex flex-col justify-end bg-linear-to-t from-black to-black/0 to-40% p-6">
+                              <p className="font-display text-base/6 font-semibold tracking-wide text-white">
+                                {person.name}
+                              </p>
+                              <p className="mt-2 text-sm text-white">
+                                {person.role}
+                              </p>
+                            </div>
                           </div>
-                        </div>
-                      </FadeIn>
+                        </FadeIn>
+                      </a>
                     </li>
                   ))}
                 </ul>
@@ -175,45 +166,45 @@ function Team() {
         ))}
       </div>
     </Container>
-  )
+  );
 }
 
 export const metadata: Metadata = {
-  title: 'About Us',
+  title: "Biz haqimizda",
   description:
-    'We believe that our strength lies in our collaborative approach, which puts our clients at the center of everything we do.',
-}
+    "We believe that our strength lies in our collaborative approach, which puts our clients at the center of everything we do.",
+};
 
 export default async function About() {
-  let blogArticles = (await loadArticles()).slice(0, 2)
+  let blogArticles = (await loadArticles()).slice(0, 2);
 
   return (
     <>
-      <PageIntro eyebrow="About us" title="Our strength is collaboration">
+      <PageIntro eyebrow="Biz haqimizda" title="Kuch birlikda!">
         <p>
-          We believe that our strength lies in our collaborative approach, which
-          puts our clients at the center of everything we do.
+          O'zbekistonda ochiq dasturiy ta'minot madaniyatini rivojlantirish va
+          dasturchilar uchun ijodiy va hamkorlik muhiti yaratishni maqsad qilgan
+          tashabbusdir.
         </p>
         <div className="mt-10 max-w-2xl space-y-6 text-base">
           <p>
-            Studio was started by three friends who noticed that developer
-            studios were charging clients double what an in-house team would
-            cost. Since the beginning, we have been committed to doing things
-            differently by charging triple instead.
+            Floss O'zbekiston aslida tarixda boshqa nomlar bilan yaralib, yillar
+            davomida rivojlanib kelgan, so'ngra hamjamiyat bilan birgalikda
+            uning qadriyatlari haqida kelishib olingach, hozirgi nom va holatga
+            keltirilgan tashkilot.
           </p>
           <p>
-            At Studio, we’re more than just colleagues — we’re a family. This
-            means we pay very little and expect people to work late. We want our
-            employees to bring their whole selves to work. In return, we just
-            ask that they keep themselves there until at least 6:30pm.
+            Bizning asosiy maqsadimiz, biz ochiq manbali dasturiy ta'minot
+            tamoyillarini ommalashtirish, bilim almashish hamda dasturchilar
+            orasida erkin va ochiq muloqotni rag'batlantirishga intilamiz.
           </p>
         </div>
       </PageIntro>
       <Container className="mt-16">
         <StatList>
-          <StatListItem value="35" label="Underpaid employees" />
-          <StatListItem value="52" label="Placated clients" />
-          <StatListItem value="$25M" label="Invoices billed" />
+          <StatListItem value="1323+" label="A'zolar soni" />
+          <StatListItem value="4" label="Hamjamiyatlar" />
+          <StatListItem value="12+" label="Loyihalar" />
         </StatList>
       </Container>
 
@@ -223,12 +214,12 @@ export default async function About() {
 
       <PageLinks
         className="mt-24 sm:mt-32 lg:mt-40"
-        title="From the blog"
-        intro="Our team of experienced designers and developers has just one thing on their mind; working on your ideas to draw a smile on the face of your users worldwide. From conducting Brand Sprints to UX Design."
+        title="Bloglarimizdan"
+        intro="Bizning tajribali dasturchi va mutaxassislar jamoamiz doimo ochiq manbali dasturlar va innovatsion yechimlarni rivojlantirish orqali jamiyatimiz rivojiga hissa qo'shishga intiladi. Texnik maqolalar, amaliy maslahatlar va dasturlash sohasidagi yangiliklar bilan sizlarni doimiy ravishda tanishtirib boramiz."
         pages={blogArticles}
       />
 
       <ContactSection />
     </>
-  )
+  );
 }
