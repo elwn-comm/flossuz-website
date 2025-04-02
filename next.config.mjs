81475bee3920f2677ad4e08e08d7d659fd11cf5c
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2025-04-02 21:42:33 +0500

    chore: ssg

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ import { unifiedConditional } from 'unified-conditional'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 }
 
